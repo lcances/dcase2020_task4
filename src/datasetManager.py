9000b7a431880472fb4a3da08f95fa3c165e05bb
leocances <markII0802>
2020-03-14 22:03:06 +0200

    backup

--- src/datasetManager.py
+++ src/datasetManager.py
@@ -1,12 +1,53 @@
-import torch
+import os
+import pandas
+
+import torch.utils.data
+
 
 class DatasetManager(torch.utils.data.Dataset):
     def __init__(self, metadata_root, audio_root):
         self.metadata_root = metadata_root
         self.audio_root = audio_root
 
+        self.meta = {
+            "train": {
+                "weak": None,
+                "unlabel_in_domain": None,
+                "synthetic20": None,
+            },
+
+            "validation": None,
+            "evaluation": None,
+        }
+
+        self.audio = {
+            "train": {
+                "weak": None,
+                "unlabel_in_domain": None,
+                "synthetic20": None,
+            },
+
+            "validaiton": None,
+            "evaluation": None,
+        }
+
+        self._load_metadata()
+
     def _load_metadata(self):
-        pass
+        # load metadata for all training dataset
+        for key in self.meta["train"]:
+            path = os.path.join(self.metadata_root, "train", key + ".tsv")
+
+            self.meta["train"][key] = pandas.read_csv(path, sep="\t")
 
     def _hdf_to_dict(self):
         pass
+
+
+if __name__ == '__main__':
+    manager = DatasetManager(
+        metadata_root="../dataset/DESED/dataset/metadata",
+        audio_root="../dataset/DESED/dataset/audio"
+    )
+
+    print(manager.meta["train"])
