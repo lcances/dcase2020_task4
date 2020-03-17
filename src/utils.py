1b730f03175290ff76d6b27792ad8d56c36ca31b
leo <leocances@gmail.com>
2020-03-17 19:19:23 +0200

    add utility funtions to set up seed qnd logging

--- src/utils.py
+++ src/utils.py
@@ -1,4 +1,9 @@
 from multiprocessing import Process, Manager
+import logging
+import datetime
+import random
+import numpy as np
+import torch
 
 
 def feature_cache(func):
@@ -61,3 +66,36 @@ def multiprocess_feature_cache(func):
     decorator.cache = decorator.manager.dict()
 
     return decorator
+
+
+def get_datetime():
+    now = datetime.datetime.now()
+    return str(now)[:10] + "_" + str(now)[11:-7]
+
+
+def get_model_from_name(model_name):
+    import models
+    import inspect
+
+    for name, obj in inspect.getmembers(models):
+        if inspect.isclass(obj) or inspect.isfunction(obj):
+            if obj.__name__ == model_name:
+                logging.info("Model loaded: %s" % model_name)
+                return obj
+    raise AttributeError("This model does not exist: %s " % model_name)
+
+
+def reset_seed(seed):
+    random.seed(seed)
+    torch.manual_seed(seed)
+    torch.cuda.manual_seed_all(seed)
+    np.random.seed(seed)
+    torch.backends.cudnn.deterministic=True
+    torch.backends.cudnn.benchmark = False
+
+
+def set_logs(loglevel):
+    numeric_level = getattr(logging, loglevel.upper(), None)
+    if not isinstance(numeric_level, int):
+        raise ValueError('Invalid log level: %s' % loglevel)
+    logging.basicConfig(level=numeric_level)
