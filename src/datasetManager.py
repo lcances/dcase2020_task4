05539cce4da6d8de5e79fe9b83ce41c2b40b7c18
leo <leocances@gmail.com>
2020-03-17 16:46:36 +0200

    add a DESEDManager allowing to load any subset from DESED

--- src/datasetManager.py
+++ src/datasetManager.py
@@ -7,14 +7,13 @@ import librosa
 
 import torch.utils.data
 
-from .utils import feature_cache, multiprocess_feature_cache
+from utils import feature_cache, multiprocess_feature_cache
 
 
 class DatasetManager(torch.utils.data.Dataset):
-    def __init__(self, metadata_root, audio_root, sampling_rate: int = 22050, preload: bool = True, verbose: int = 1):
+    def __init__(self, metadata_root, audio_root, sampling_rate: int = 22050, verbose: int = 1):
         self.metadata_root = metadata_root
         self.audio_root = audio_root
-        self.preload = preload
         self.verbose = verbose
         self.sampling_rate = sampling_rate
 
@@ -51,7 +50,6 @@ class DatasetManager(torch.utils.data.Dataset):
         }
 
         self._load_metadata()
-        self._load_audio()
 
     def _load_metadata(self):
         # load metadata for all training dataset
@@ -65,21 +63,21 @@ class DatasetManager(torch.utils.data.Dataset):
             self.meta["train"][key] = df
 
     def _load_audio(self):
-        hdf_file = h5py.File(self.hdf_dataset, "r")
-
         # load raw audio for all training set
         for key in self.meta["train"]:
-            self.load_subset(hdf_file, "train", key)
+            self.load_subset("train", key)
 
-        hdf_file.close()
+    def load_subset(self, dataset: str, subset: str = None):
+        hdf_file = h5py.File(self.hdf_dataset, "r")
 
-    def load_subset(self, hdf_file, dataset: str, subset: str = None):
         if subset is not None:
             path = os.path.join(self.audio_root, dataset, subset)
             self.audio[dataset][subset] = self._hdf_to_dict(hdf_file, path)
         else:
             path = os.path.join(self.audio_root, dataset)
             self.audio[dataset] = self._hdf_to_dict(hdf_file, path)
+
+        hdf_file.close()
 
     def _hdf_to_dict(self, hdf_file, path: str):
         print(path)
@@ -110,10 +108,60 @@ class DatasetManager(torch.utils.data.Dataset):
         feat = librosa.power_to_db(feat, ref=np.max)
         return feat
 
-if __name__ == '__main__':
-    manager = DatasetManager(
-        metadata_root="../dataset/DESED/dataset/metadata",
-        audio_root="../dataset/DESED/dataset/audio"
-    )
 
-    print(manager.meta["train"])
+class DESEDManager(DatasetManager):
+    def __init__(self, metadata_root, audio_root, sampling_rate: int = 22050, verbose: int = 1,
+            subset: str = "weak", validation: bool = False, validation_ratio: float = 0.2):
+
+        super().__init__(metadata_root, audio_root, sampling_rate, verbose)
+        
+        self.subset = subset
+        self.validation_ratio = validation_ratio
+
+        # load only the concern audio
+        self.load_subset("train", self.subset)
+
+        # prepare the variables
+        self.X = self.audio["train"][self.subset]
+        self.y = self.audio["train"][self.subset]
+
+        self.filenames = self.X.keys()
+
+        # if it is a validation set
+        self._prepare_validation()
+
+    def _prepare_validation(self):
+        # count how many file is in the validation fold
+        nb_file = len(self.filenames)
+        nb_validation = nb_file // self.validation_ratio
+
+        # pick it from the filename list
+        validation_filenames = np.random.choice(nb_validation, size=nb_validation)
+
+        # remove other file from the dictionnary to free memory
+        for name in self.X:
+            if name not in validation_filenames:
+                self.X.pop(name, None)
+
+        # remove other file from the metadata
+        for name in self.y.filename.value:
+            if name not in validation_filenames:
+                self.y.drop(name, inplace=True)
+
+        
+
+
+
+if __name__ == '__main__':
+    metadata_root="../dataset/DESED/dataset/metadata",
+    audio_root="../dataset/DESED/dataset/audio"
+
+    train_weak_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="weak", validation=False)
+    val_weak_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="weak", validation=True, validation_ratio=0.2)
+
+    train_uid_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="unlabel_in_domain", validation=False)
+    val_uid_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="unlabel_in_domain", validation=True, validation_ratio=0.2)
+
+    train_synth_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="synthetic20", validation=False)
+    val_synth_dataset = DESEDManager(metadata_root, audio_root, 22050, subset="synthetic20", validation=True, validation_ratio=0.2)
+
