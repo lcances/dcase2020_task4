a178d9cddeb7a7f5fe74814a38c1305fea5ff075
leocances <leocances@gmail.com>
2020-04-02 11:12:37 +0200

    add chunk size loading

--- standalone/move_to_hdf.py
+++ standalone/move_to_hdf.py
@@ -16,6 +16,7 @@ parser.add_argument("+DESED", action="store_true", default=False)
 parser.add_argument("+FUSS", action="store_true", default=False)
 parser.add_argument("--num_workers", default=4, type=int)
 parser.add_argument("--compression", default=None, type=str)
+parser.add_argument("--chunk_size", default=100, type=int, help="Limit the number of file store in memory before writing to hdf")
 args = parser.parse_args()
 
 SR = args.sampling_rate
@@ -46,7 +47,7 @@ to_remove = [".DS_Store"]
 
 DESED_directories = [
     os.path.join("DESED", "dataset", "audio", "train", "weak"),
-    os.path.join("DESED", "dataset", "audio", "train", "unlabel_in_domain"),
+    os.path.join("DESED", "dataset", "audio", "train", "unlabel_in_domain"), # TODO this directory contain 14 Go of file. So far it can lead to memory error with computer equiped with less than 16 Go
     os.path.join("DESED", "dataset", "audio", "train", "synthetic20"),
     os.path.join("DESED", "dataset", "audio", "validation"),
 ]
@@ -81,29 +82,31 @@ for directory in tqdm.tqdm(directory_to_load):
     print("loading : %s" % directory)
     folder_path = os.path.join("../dataset", directory)
 
-    # prepare the group information and coresponding dataset
+    # prepare the group information and corresponding dataset
     print("folder_path: ", folder_path)
     file_list = os.listdir(folder_path)
     file_list = [name for name in file_list if name not in to_remove and name[-3:] == "wav"]
-    dataset_shape = (len(file_list), SR*LENGTH)
 
     # for every file of the directory, extract the raw audio
     raw_audios = []
     workers = Pool(args.num_workers)
     folder_path_duplicate = [folder_path] * len(file_list)
 
-    results = workers.starmap(load_file, zip(folder_path_duplicate, file_list))
+    # Create the HDF group and prepare the final size
+    dataset_shape = (len(file_list), SR * LENGTH)
 
-    results = np.array(results)
-    file_list = np.array(file_list)
-
-    # Create the hdf group and write the dataset
-    print("folder_path : ", folder_path)
     hdf_fold = hdf.create_group(folder_path)
-    hdf_fold.create_dataset("data", data=results, compression=args.compression)
+    hdf_fold.create_dataset("data", dataset_shape, compression=args.compression)
     hdf_fold.create_dataset("filenames", (len(file_list), ), dtype=h5py.special_dtype(vlen=str))
     for i in range(len(file_list)):
         hdf_fold["filenames"][i] = file_list[i]
+
+    for index in range(0, dataset_shape[0] - args.chunk_size, args.chunk_size):
+
+        results = workers.starmap(load_file, zip(folder_path_duplicate[index:index+args.chunk_size], file_list[index:index+args.chunk_size]))
+
+        # add the chunk to the hdf file
+        hdf_fold["data"][index:index+1000] = results
 
     workers.close()
     workers.join()
