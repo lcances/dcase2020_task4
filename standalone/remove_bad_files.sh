36653b68744c363f5704b07fbfe29069870a1bfa
leocances <leocances@gmail.com>
2020-04-02 11:48:31 +0200

    made it a script for easier use

---
+++ standalone/remove_bad_files.sh
@@ -0,0 +1,7 @@
+local_dir=`pwd`
+
+echo "Removing bad files from DESED / train /unlabel in domain"
+cd ../dataset/DESED/dataset/audio/train/unlabel_in_domain/
+rm Yh2iKe6kB-2Y_10.000_20.000.wav
+
+cd $PWD
