1a1e9dc7f0af3cc286057b31fea84acab6648271
leocances <leocances@gmail.com>
2020-04-03 10:07:10 +0200

    basic model for weak supervised training

--- dcase2020/models.py
+++ dcase2020/models.py
@@ -5,20 +5,20 @@ from dcase2020.layers import ConvBNReLUPool, ConvPoolReLU
 
 class WeakBaseline(nn.Module):
     def __init__(self, **kwargs):
-        super(WeakBaseline, self).__init__()
+        nn.Module.__init__(self)
 
         self.features = nn.Sequential(
             ConvPoolReLU(1, 32, 3, 1, 1, pool_kernel_size=(4, 2), pool_stride=(4, 2), dropout=0.0),
             ConvPoolReLU(32, 32, 3, 1, 1, pool_kernel_size=(4, 2), pool_stride=(4, 2), dropout=0.3),
             ConvPoolReLU(32, 32, 3, 1, 1, pool_kernel_size=(4, 2), pool_stride=(4, 2), dropout=0.3),
-            nn.Conv2d(32, 32, 1, 1, 1),
+            nn.Conv2d(32, 32, 1, 1, 0),
             nn.ReLU6(),
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
             nn.Dropout(0.5),
-            nn.Linear() # TODO fill
+            nn.Linear(1696, 10) # TODO fill
         )
 
     def forward(self, x):
