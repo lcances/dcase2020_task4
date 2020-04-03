f3a6944333c4c93f353eeb957b9a67e3d4fd2a1b
leocances <leocances@gmail.com>
2020-04-03 10:33:00 +0200

    update gitignore and add a basic installation script

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='dcase2020',
+    version='1.0.0',
+    url='https://github.com/leocances/dcase2020.git',
+    author='Leo Cances',
+    author_email='leocances@gmail.com',
+    description='For the dcase 2020 task 4 challenge',
+    packages=find_packages(),    
+)
