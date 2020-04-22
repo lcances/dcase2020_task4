d707066b70b06d926ad4849e1be2e05021bfa9ee
leocances <leocances@gmail.com>
2020-04-22 11:26:37 +0200

    refactor

--- tests/test_datasetManager.py
+++ tests/test_datasetManager.py
@@ -2,7 +2,7 @@
 Not proper testing, but actual functional test
 """
 
-from .src.datasetManager import DESEDManager
+from dcase2020.datasetManager import DESEDManager
 
 metadata_root="../dataset/DESED/dataset/metadata"
 audio_root="../dataset/DESED/dataset/audio"
@@ -11,8 +11,8 @@ manager = DESEDManager(metadata_root, audio_root, 22050, validation_ratio=0.2, v
 
 print("Loading all subsets for training")
 manager.add_subset("weak")
-manager.add_subset("unlabel_in_domain")
-manager.add_subset("synthetic20")
+#manager.add_subset("unlabel_in_domain")
+#manager.add_subset("synthetic20")
 
 print("spliting 80 / 20 % train / validation")
 manager.split_train_validation()
