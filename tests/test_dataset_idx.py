a60e0ee3976e07d2d1a8c1b506c49af817796b1a
Labbeti <etienne.labbe31@gmail.com>
2020-05-29 19:31:56 +0200

    Add: Test for dataset indexes.

---
+++ tests/test_dataset_idx.py
@@ -0,0 +1,23 @@
+from dcase2020_task4.util.dataset_idx import split_classes_idx
+
+
+def test():
+	tests = [
+		([[1, 2], [3, 4], [5, 6]], [0.5, 0.5]),
+		([[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12]], [0.5, 0.25, 0.25]),
+	]
+	expected_lst = [
+		[[1, 3, 5], [2, 4, 6]],
+		[[1, 2, 5, 6, 9, 10], [3, 7, 11], [4, 8, 12]],
+	]
+
+	for (indices, ratios), expected in zip(tests, expected_lst):
+		c = split_classes_idx(indices, ratios)
+		if c == expected:
+			print("OK")
+		else:
+			print("KO")
+
+
+if __name__ == "__main__":
+	test()
