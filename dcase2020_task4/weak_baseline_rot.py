e701726e00202bc1a1d66da08fd82e8219495ab9
Labbeti <etienne.labbe31@gmail.com>
2020-06-02 23:31:02 +0200

    Add: WeakBaselineRot class for running ReMixMatch on DESED.

---
+++ dcase2020_task4/weak_baseline_rot.py
@@ -0,0 +1,21 @@
+
+from torch import nn, Tensor
+from dcase2020_task4.baseline.models import WeakBaseline
+
+
+class WeakBaselineRot(WeakBaseline):
+	def __init__(self, nb_rot: int = 4):
+		super().__init__()
+
+		self.classifier_rot = nn.Sequential(
+			nn.Flatten(),
+			nn.Linear(1696, nb_rot)
+		)
+
+	def forward_rot(self, x: Tensor) -> Tensor:
+		x = x.view(-1, 1, *x.shape[1:])
+
+		x = self.features(x)
+		x = self.classifier_rot(x)
+
+		return x
