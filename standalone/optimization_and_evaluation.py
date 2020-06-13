2192803cd4557a453b2edabc5bbe186e4ab2e18c
leocances <leocances@gmail.com>
2020-06-13 22:07:56 +0200

    Optimization script et evaluation

--- standalone/optimization_and_evaluation.py
+++ standalone/optimization_and_evaluation.py
@@ -1,11 +1,5 @@
-
-import time
 import os
-from pathlib import Path
-import torch.nn as nn
-import torch.nn.functional as F
 import torch.utils.data
-from torch.utils.tensorboard import SummaryWriter
 
 import sys
 sys.path.append("..")
@@ -19,19 +13,25 @@ from dcase2020.datasetManager import DESEDManager
 from dcase2020.datasets import DESEDDataset
 
 # utility function & metrics & augmentation
-from metric_utils.metrics import FScore, BinaryAccuracy
-from dcase2020_task4.util.utils import get_datetime, reset_seed
+from metric_utils.metrics import FScore
+from dcase2020_task4.util.utils import reset_seed, get_model_from_name
 
 # All models available
 from dcase2020_task4.baseline.models import *
-from dcase2020_task4.dcase2019
+from dcase2020_task4.dcase2019.models import *
+
+# for strong output optimization
+from aeseg.Encoder import Encoder
+from aeseg.optimizers import GenOptimizer, DichotomicOptimizer
+from aeseg.aeseg import eb_evaluator, sb_evaluator
 
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--model", default="../models/dcase2019_system.torch", help="Path to model save using checkpoint")
-parser.add_argument("--model_name", default="../models/dcase2019_system.torch", help="Path to model save using checkpoint")
+parser.add_argument("--model_save", default="../models/dcase2019_system.torch", help="Path to model save using checkpoint")
+parser.add_argument("--model_name", default="dcase2019_system", help="Name of the Class/function to use to construct the model")
 parser.add_argument("-a", "--audio_root", default="../../dataset/DESED/dataset/audio", type=str)
 parser.add_argument("-m", "--metadata_root", default="../../dataset/DESED/dataset/metadata", type=str)
+parser.add_argument("-w", "--num_workers", default=0, type=int, help="Choose number of worker to train the model")
 args = parser.parse_args()
 
 
@@ -60,31 +60,34 @@ manager = DESEDManager(
 )
 
 
-# # Prepare dataset and model
+# ======================================================================================================================
+# PREPARE DATASET AND MODEL
+# ======================================================================================================================
 manager.add_subset("validation")
 val_dataset = DESEDDataset(manager, train=False, val=True, weak=True, strong=True, augments=[], cached=True)
 val_loader = torch.utils.data.DataLoader(val_dataset, batch_size=64, shuffle=False)
 
 
-best_model = dcase2019_model()
+model_func = get_model_from_name(args.model_name)
+best_model = model_func()
 best_model.cuda()
 best_model.eval() # <-- for consistency in scoring (deactivate dropout and batchNorm)
+log.info("Model %s loaded" % args.model_name)
 
-checkpoint = torch.load("../models/best_dcase2019.torch")
+checkpoint = torch.load(args.model_save)
 best_model.load_state_dict(checkpoint["state_dict"])
+log.info("Best state reach at epoch %d with score %.2f" % (checkpoint["epoch"], checkpoint["best_metric"]))
 
 
-# ## Perform prediction on validation dataset
-
-# In[11]:
-
-
+# ======================================================================================================================
+# PREDICTION ON VALIDATION DATASET
+# ======================================================================================================================
 weak_y_true, strong_y_true = None, None
 weak_y_pred, strong_y_pred = None, None
 y_filenames = list(val_dataset.X.keys())
 
 with torch.set_grad_enabled(False):
-    for i, (X, y) in enumerate(val_loader):
+    for i, (X, y) in tqdm(enumerate(val_loader)):
         weak_y, strong_y = y
         weak_y, strong_y = weak_y.cuda(), strong_y.cuda()
         X = X.cuda()
@@ -107,26 +110,18 @@ with torch.set_grad_enabled(False):
 
             weak_y_pred =  torch.cat((weak_y_pred, weak_pred.cpu()), dim=0)
             strong_y_pred =  torch.cat((strong_y_pred, strong_pred.cpu()), dim=0)
-        
-        print(" %d %%" % int(100 * (i + 1) / len(val_loader)), end="\r")
 
 
-# In[12]:
-
-
-weak_y_true.shape, weak_y_pred.shape, strong_y_true.shape, strong_y_pred.shape, len(y_filenames)
-
-
-# # Audio tagging threshold Optimization
-
-# In[13]:
-
-
+# ======================================================================================================================
+# audio tagging threshold optimization
+# ======================================================================================================================
 classwise_f1 = FScore(dim=0)
+
+
 def simulated_anealing(y_pred, y_true, macro_iteration = 50, micro_iteration = 400):
     weak_y_pred_ = y_pred.clone().detach()
 
-    classwise_f1(weak_y_pred_, weak_y_true)
+    classwise_f1(weak_y_pred_, y_true)
     start_f1 = classwise_f1.value
 
     min_delta = 10e-7
@@ -144,7 +139,7 @@ def simulated_anealing(y_pred, y_true, macro_iteration = 50, micro_iteration = 4
     best_f1 = start_f1.clone().detach()
 
     total_iteration = macro_iteration * micro_iteration
-    
+
     for M in range(macro_iteration):
         thresholds = torch.ones(10) * 0.5
         delta_ratio = 0.3
@@ -165,65 +160,33 @@ def simulated_anealing(y_pred, y_true, macro_iteration = 50, micro_iteration = 4
             weak_y_pred_[bin_y_pred <= new_thresholds] = 0
 
             # calc new f1
-            classwise_f1(weak_y_pred_, weak_y_true)
+            classwise_f1(weak_y_pred_, y_true)
             new_f1 = classwise_f1.value
             history["f1"].append(new_f1)
 
             # check
             for i in range(10):
                 if new_f1[i] > best_f1[i]:
-#                     print(new_f1[i], " > ", best_f1[i])
                     best_f1[i] = new_f1[i]
                     best_thresholds[i] = new_thresholds[i]
                     thresholds[i] = best_thresholds[i]
                     history["best_f1"][i].append(best_f1[i])
-#                     print(best_thresholds)
 
             step = M * micro_iteration + m
             print("%2.2f / 100.0 (%%)" % ((step / total_iteration) * 100), end="\r")
             
     return start_f1, best_f1, best_thresholds, history
 
-
-# In[14]:
-
-
+log.info("Start tagging optimization")
 initial_f1, best_f1, best_at_thresholds, history = simulated_anealing(weak_y_pred, weak_y_true, 20, 400)
 
+log.info("Previous macro F1 score: %.2f - New macro F1 score: %.2f" % (torch.mean(initial_f1), torch.mean(best_f1)))
 
-# In[15]:
-
-
-torch.mean(initial_f1), torch.mean(best_f1)
-
-
-# In[16]:
-
-
-best_at_thresholds
-
-
-# # pruning of strong prediction
-
-# ## Apply best tagging threshold
-
-# In[17]:
-
-
+# ==== pruning of strong prediction ====
+log.info("Pruning strong prediction ...")
 best_weak_y_pred = weak_y_pred.clone().detach()
-
 best_weak_y_pred[best_weak_y_pred > best_at_thresholds] = 1
 best_weak_y_pred[best_weak_y_pred <= best_at_thresholds] = 0
-
-
-# In[18]:
-
-
-best_weak_y_pred.shape, strong_y_pred.shape
-
-
-# In[19]:
-
 
 # Prune the result of fill the missing curve with 0 segments
 def prune_prediction(strong_y_pred, weak_y_pred):
@@ -240,71 +203,22 @@ def prune_prediction(strong_y_pred, weak_y_pred):
                 
     return pruned_strong_y_pred
 
-
-# In[20]:
-
-
 pruned_strong_y_pred = prune_prediction(strong_y_pred, best_weak_y_pred)
 
 
-# ### Sanity check and visualization
-
-# In[21]:
 
 
-best_weak_y_pred[1], pruned_strong_y_pred[1]
 
-
-# In[23]:
-
-
-import matplotlib.pyplot as plt
-
-class_list = list(DE.cls_dict.keys())
-plt.figure(0, figsize=(15, 6))
-
-for i, class_name in enumerate(class_list):
-    plt.plot(pruned_strong_y_pred[1][i], label=class_name)
-
-plt.legend()
-plt.show()
-
-
-# # localization Optimization
-
-# In[24]:
-
-
-# pip install -i https://test.pypi.org/simple/ aeseg
-
-
-# In[25]:
-
-
-from aeseg.Encoder import Encoder
-from aeseg.optimizers import GenOptimizer, DichotomicOptimizer
-from aeseg.core import eb_evaluator, sb_evaluator
-
-
-# In[26]:
-
-
+# ======================================================================================================================
+# AUDIO LOC OPTIMIZATION
+# ======================================================================================================================
 def load_csv(path):
     with open(path, "r") as f:
         data = f.read().splitlines()[1:]
     return data
 
-
-# In[27]:
-
-
-## Optimizer take data in the following shape (file, seq, class)
 strong_y_true = strong_y_true.permute(0, 2, 1)
 pruned_strong_y_pred = pruned_strong_y_pred.permute(0, 2, 1)
-strong_y_true.shape, pruned_strong_y_pred.shape
-
-
-# In[ ]:
 
 
 def class_wise_optimization(optimizer, class_to_monitor, strong_y_true, strong_pred, filenames, method="threshold"):
@@ -338,12 +252,9 @@ def get_class_dependant_parameters(class_wise_results, class_list):
     return class_dependant_parameters
 
 
-# In[ ]:
+class_list = list(DESEDManager.cls_dict.keys())
 
-
-class_list = list(DE.cls_dict.keys())
-
-# Create the encoder that will be used
+# ==== Create the encoder with challenge parameters ====
 encoder = Encoder(
     classes=class_list,
     temporal_precision = 50,  # ms
@@ -353,84 +264,39 @@ encoder = Encoder(
 
 val_csv_y_true= load_csv(os.path.join(desed_metadata_root, "validation", "validation.tsv"))
 
-
-# ## Absolute threshold
-
-# In[ ]:
-
-
-optimizer = DichotomicOptimizer(
-    {
-        "threshold": (0.01, 0.90),
-        "smooth": "smoothMovingAvg",
-        "window_len": (5, 27)
-    },
-    
-    encoder = encoder,
-    step = 6,
-    nb_recurse = 8,
-    nb_process = 5
-)
-
-class_wise_results = class_wise_optimization(
-    optimizer,
-    class_list,
-    method="threshold",
-    strong_y_true=val_csv_y_true,
-    strong_pred=pruned_strong_y_pred.numpy(),
-    filenames=y_filenames
-)
-
-
 # ## Hysteresis thresholding
-
-# In[241]:
-
-
-# Perform the optimization ON TEST
-# Create the optimizer
 optimizer = DichotomicOptimizer(
     {
         "high": (0.01, 0.99),
         "low": (0.01, 0.99),
-#         "smooth": "smoothMovingAvg",
+        "smooth": "smoothMovingAvg",
         "window_len": (3, 27)
     },
     
     encoder = encoder,
     step = 6,
     nb_recurse = 8,
-    nb_process = 5
+    nb_process = args.num_workers
 )
 
 class_wise_results = class_wise_optimization(
     optimizer,
     class_list,
     method="hysteresis",
-    strong_y_true=strong_y_true,
+    strong_y_true=val_csv_y_true,
     strong_pred=pruned_strong_y_pred.numpy(),
     filenames=y_filenames
 )
 
 
-# In[87]:
-
-
-best_parameters = get_class_dependant_parameters(class_wise_results, class_list)
-
-
-# In[88]:
-
-
-# check test
 best_parameters = get_class_dependant_parameters(class_wise_results, class_list)
 
 segments = encoder.encode(
-    val_strong_pred,
+    pruned_strong_y_pred.numpy(),
     method="hysteresis",
     **best_parameters
 )
-to_evaluate = encoder.parse(segments, val_filenames)
+to_evaluate = encoder.parse(segments, y_filenames)
 evaluator = eb_evaluator(val_csv_y_true, to_evaluate)
 print(evaluator)
 
