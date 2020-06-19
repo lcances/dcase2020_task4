911ae26a8fa3fda7fc6287cb7d1bc767a3bd2888
Labbeti <etienne.labbe31@gmail.com>
2020-06-19 16:11:58 +0200

    Add: MixMatch loc trainer and loss.

---
+++ dcase2020_task4/mixmatch/losses/tag_loc.py
@@ -0,0 +1,42 @@
+
+from torch import Tensor
+from torch.nn.functional import binary_cross_entropy
+
+from typing import Callable
+
+
+class MixMatchLossMultiHotLoc(Callable):
+	""" MixMatch loss component. """
+
+	def __init__(self, lambda_u: float = 1.0):
+		self.lambda_u = lambda_u
+
+		self.criterion_s_weak = binary_cross_entropy
+		self.criterion_u_weak = binary_cross_entropy
+		self.criterion_s_strong = binary_cross_entropy
+		self.criterion_u_strong = binary_cross_entropy
+
+	@staticmethod
+	def from_edict(hparams) -> 'MixMatchLossMultiHotLoc':
+		return MixMatchLossMultiHotLoc(hparams.lambda_u)
+
+	def __call__(
+		self,
+		s_pred_weak: Tensor, s_target_weak: Tensor, s_pred_strong: Tensor, s_target_strong: Tensor,
+		u_pred_weak: Tensor, u_target_weak: Tensor, u_pred_strong: Tensor, u_target_strong: Tensor,
+	) -> (Tensor, Tensor, Tensor, Tensor, Tensor):
+		loss_s_weak = self.criterion_s_weak(s_pred_weak, s_target_weak)
+		loss_s_weak = loss_s_weak.mean()
+
+		loss_s_strong = self.criterion_s_strong(s_pred_strong, s_target_strong)
+		loss_s_strong = loss_s_strong.mean()
+
+		loss_u_weak = self.criterion_u_weak(u_pred_weak, u_target_weak)
+		loss_u_weak = loss_u_weak.mean()
+
+		loss_u_strong = self.criterion_u_strong(u_pred_strong, u_target_strong)
+		loss_u_strong = loss_u_strong.mean()
+
+		loss = loss_s_weak + loss_s_strong + self.lambda_u * (loss_u_weak + loss_u_strong)
+
+		return loss, loss_s_weak, loss_u_weak, loss_s_strong, loss_u_strong
--- dcase2020_task4/mixmatch/trainer_loc.py
+++ dcase2020_task4/mixmatch/trainer_loc.py
@@ -15,7 +15,7 @@ from dcase2020_task4.util.utils_match import get_lr
 from dcase2020_task4.metrics_values_buffer import MetricsValuesBuffer
 
 
-class MixMatchTrainer(SSTrainerABC):
+class MixMatchTrainerLoc(SSTrainerABC):
 	def __init__(
 		self,
 		model: Module,
@@ -23,8 +23,10 @@ class MixMatchTrainer(SSTrainerABC):
 		optim: Optimizer,
 		loader_train_s_augm: DataLoader,
 		loader_train_u_augms: DataLoader,
-		metrics_s: Dict[str, Metrics],
-		metrics_u: Dict[str, Metrics],
+		metrics_s_weak: Dict[str, Metrics],
+		metrics_u_weak: Dict[str, Metrics],
+		metrics_s_strong: Dict[str, Metrics],
+		metrics_u_strong: Dict[str, Metrics],
 		criterion: Callable,
 		writer: Optional[SummaryWriter],
 		mixer: Callable,
@@ -35,17 +37,21 @@ class MixMatchTrainer(SSTrainerABC):
 		self.optim = optim
 		self.loader_train_s_augm = loader_train_s_augm
 		self.loader_train_u_augms = loader_train_u_augms
-		self.metrics_s = metrics_s
-		self.metrics_u = metrics_u
+		self.metrics_s_weak = metrics_s_weak
+		self.metrics_u_weak = metrics_u_weak
+		self.metrics_s_strong = metrics_s_strong
+		self.metrics_u_strong = metrics_u_strong
 		self.criterion = criterion
 		self.writer = writer
 		self.mixer = mixer
 		self.lambda_u_rampup = lambda_u_rampup
 
 		self.metrics_values = MetricsValuesBuffer(
-			list(self.metrics_s.keys()) +
-			list(self.metrics_u.keys()) +
-			["loss", "loss_s", "loss_u"]
+			list(self.metrics_s_weak.keys()) +
+			list(self.metrics_u_weak.keys()) +
+			list(self.metrics_s_strong.keys()) +
+			list(self.metrics_u_strong.keys()) +
+			["loss", "loss_s_weak", "loss_u_weak", "loss_s_strong", "loss_u_strong"]
 		)
 
 	def train(self, epoch: int):
@@ -57,26 +63,32 @@ class MixMatchTrainer(SSTrainerABC):
 		iter_train = iter(loaders_zip)
 
 		for i, item in enumerate(iter_train):
-			(s_batch_augm, s_labels_weak), u_batch_augms = item
+			(s_batch_augm, s_labels_weak, s_labels_strong), u_batch_augms = item
 
 			s_batch_augm = s_batch_augm.cuda().float()
 			s_labels_weak = s_labels_weak.cuda().float()
+			s_labels_strong = s_labels_strong.cuda().float()
 			u_batch_augms = torch.stack(u_batch_augms).cuda().float()
 
 			# Apply mix
-			s_batch_mixed, s_labels_mixed, u_batch_mixed, u_labels_mixed = self.mixer(
-				s_batch_augm, s_labels_weak, u_batch_augms
+			s_batch_mixed, s_labels_weak_mixed, s_labels_strong_mixed, u_batch_mixed, u_labels_weak_mixed, u_labels_strong_mixed = self.mixer(
+				s_batch_augm, s_labels_weak, s_labels_strong, u_batch_augms
 			)
 
 			# Compute logits
-			s_logits_mixed = self.model(s_batch_mixed)
-			u_logits_mixed = self.model(u_batch_mixed)
+			s_logits_weak_mixed, s_logits_strong_mixed = self.model(s_batch_mixed)
+			u_logits_weak_mixed, u_logits_strong_mixed = self.model(u_batch_mixed)
 
-			s_pred_mixed = self.acti_fn(s_logits_mixed, dim=1)
-			u_pred_mixed = self.acti_fn(u_logits_mixed, dim=1)
+			s_pred_weak_mixed = self.acti_fn(s_logits_weak_mixed, dim=1)
+			u_pred_weak_mixed = self.acti_fn(u_logits_weak_mixed, dim=1)
+			s_pred_strong_mixed = self.acti_fn(s_logits_strong_mixed, dim=1)
+			u_pred_strong_mixed = self.acti_fn(u_logits_strong_mixed, dim=1)
 
 			# Update model
-			loss, loss_s, loss_u = self.criterion(s_pred_mixed, s_labels_mixed, u_pred_mixed, u_labels_mixed)
+			loss, loss_s_weak, loss_u_weak, loss_s_strong, loss_u_strong = self.criterion(
+				s_pred_weak_mixed, s_labels_weak_mixed, s_pred_strong_mixed, s_labels_strong_mixed,
+				u_pred_weak_mixed, u_labels_weak_mixed, u_pred_strong_mixed, u_labels_strong_mixed
+			)
 			self.optim.zero_grad()
 			loss.backward()
 			self.optim.step()
@@ -87,12 +99,16 @@ class MixMatchTrainer(SSTrainerABC):
 
 				# Compute metrics
 				self.metrics_values.add_value("loss", loss.item())
-				self.metrics_values.add_value("loss_s", loss_s.item())
-				self.metrics_values.add_value("loss_u", loss_u.item())
+				self.metrics_values.add_value("loss_s_weak", loss_s_weak.item())
+				self.metrics_values.add_value("loss_u_weak", loss_u_weak.item())
+				self.metrics_values.add_value("loss_s_strong", loss_s_strong.item())
+				self.metrics_values.add_value("loss_u_strong", loss_u_strong.item())
 
 				metrics_preds_labels = [
-					(self.metrics_s, s_pred_mixed, s_labels_mixed),
-					(self.metrics_u, u_pred_mixed, u_labels_mixed),
+					(self.metrics_s_weak, s_pred_weak_mixed, s_labels_weak_mixed),
+					(self.metrics_u_weak, u_pred_weak_mixed, u_labels_weak_mixed),
+					(self.metrics_s_strong, s_pred_strong_mixed, s_labels_strong_mixed),
+					(self.metrics_u_strong, u_pred_strong_mixed, u_labels_strong_mixed),
 				]
 				self.metrics_values.apply_metrics(metrics_preds_labels)
 				self.metrics_values.print_metrics(epoch, i, len(loaders_zip))
@@ -110,7 +126,7 @@ class MixMatchTrainer(SSTrainerABC):
 		return len(self.loader_train_u_augms) * self.loader_train_u_augms.batch_size
 
 	def reset_metrics(self):
-		metrics_dict_lst = [self.metrics_s, self.metrics_u]
+		metrics_dict_lst = [self.metrics_s_weak, self.metrics_u_weak]
 		for metrics in metrics_dict_lst:
 			for metric in metrics.values():
 				metric.reset()
