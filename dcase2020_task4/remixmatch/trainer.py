Fix: Add clamp for rotation loss of ReMixMatch.

dcase2020_task4/remixmatch/trainer.py
@@ -24,6 +24,7 @@ class ReMixMatchTrainer(SSTrainerABC):
 		self,
 		model: Module,
 		acti_fn: Callable,
+		acti_rot_fn: Callable,
 		optim: Optimizer,
 		loader_train_s: DataLoader,
 		loader_train_u: DataLoader,
@@ -56,7 +57,7 @@ class ReMixMatchTrainer(SSTrainerABC):
 		self.distributions = distributions
 		self.rot_angles = rot_angles
 
-		self.acti_fn_rot = lambda batch, dim: batch.softmax(dim=dim)
+		self.acti_rot_fn = acti_rot_fn
 		self.metrics_recorder = MetricsRecorder(
 			"train/",
 			list(self.metrics_s.keys()) +
@@ -106,7 +107,7 @@ class ReMixMatchTrainer(SSTrainerABC):
 
 			# Predict rotation for strong augment u1
 			r_logits = self.model.forward_rot(u1_batch_rotated)
-			r_pred = self.acti_fn_rot(r_logits, dim=1)
+			r_pred = self.acti_rot_fn(r_logits, dim=1)
 
 			# Update model
 			loss, loss_s, loss_u, loss_u1, loss_r = self.criterion(

standalone/match_multihot_tag.py
@@ -395,6 +395,8 @@ def main():
 			args.sharpen_threshold_multihot,
 		)
 
+		acti_rot_fn = lambda batch, dim: batch.softmax(dim=dim).clamp(min=2e-30)
+
 		if args.write_results:
 			writer = build_writer(args, suffix="%s_%d_%d_%.2f_%.2f_%.2f_%s" % (
 				suffix_tag, args.batch_size_s, args.batch_size_u, args.lambda_u, args.lambda_u1, args.lambda_r, args.suffix))
@@ -402,7 +404,7 @@ def main():
 			writer = None
 
 		trainer = ReMixMatchTrainer(
-			model, acti_fn, optim, loader_train_s_augm_strong, loader_train_u_augms_weak_strongs, metrics_s, metrics_u,
+			model, acti_fn, acti_rot_fn, optim, loader_train_s_augm_strong, loader_train_u_augms_weak_strongs, metrics_s, metrics_u,
 			metrics_u1, metrics_r, criterion, writer, mixer, distributions, rot_angles
 		)
 		validator = DefaultValidator(