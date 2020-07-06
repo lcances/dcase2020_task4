45dddcf22265b9e7611390642e541b2885460735
Labbeti <elabbe@co2-slurm-ng04.irit.fr>
2020-07-06 23:11:11 +0200

    Mod: Update scripts.

--- osirim_labbeti/cifar10.sh
+++ osirim_labbeti/cifar10.sh
@@ -12,4 +12,4 @@ nb_classes=10
 run=$1
 suffix=$2
 
-$path_py $path_script --dataset $path_dataset --dataset_name $dataset_name --nb_classes $nb_classes --logdir $path_board --debug False --model_name "VGG11" --batch_size_s 64 --batch_size_u 64 --num_workers_s 4 --num_workers_u 4 --nb_epochs 10 --write_results False --use_rampup True --cross_validation False --run "$run" --suffix "$suffix"
+$path_py $path_script --dataset $path_dataset --dataset_name $dataset_name --nb_classes $nb_classes --logdir $path_board --debug False --model_name "VGG11" --batch_size_s 64 --batch_size_u 64 --num_workers_s 4 --num_workers_u 4 --nb_epochs 10 --lambda_u 1.0 --history_size 128 --write_results False --use_rampup True --cross_validation False --run "$run" --suffix "$suffix"
