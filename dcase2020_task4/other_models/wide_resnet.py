Mod/Fix: Rename old files for distinct scripts names for img and spec augmentations. Also fix WideResNet28Rot rotation classifier.

dcase2020_task4/other_models/wide_resnet.py
@@ -97,7 +97,7 @@ class WideResNetRot(WideResNet):
 		self, depth: int, num_classes: int = 10, widen_factor: int = 1, dropout: float = 0.0, rot_output_size: int = 4
 	):
 		super().__init__(depth, num_classes, widen_factor, dropout)
-		classifier_input_size = 32 * widen_factor
+		classifier_input_size = 64 * widen_factor
 		self.classifier_rot = nn.Linear(classifier_input_size, rot_output_size)
 
 	def forward_rot(self, x: Tensor) -> Tensor:

tests/test_img_augmentations.py
@@ -1,7 +1,7 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from dcase2020_task4.util.other_augments import Inversion, UniColor, RandCrop, Gray
+from dcase2020_task4.util.other_img_augments import Inversion, UniColor, CutOut, Gray
 
 
 def get_demo_image():
@@ -14,7 +14,7 @@ def get_demo_image():
 def test():
 	img = get_demo_image()
 
-	augms = [Inversion(), UniColor(), RandCrop(), Gray()]
+	augms = [Inversion(), UniColor(), CutOut(), Gray()]
 
 	plt.imshow(np.array(img.T, dtype=int))
 	for augm in augms: