5980a954ef4064094cac608eac43597043d7b12c
Labbeti <etienne.labbe31@gmail.com>
2020-08-12 00:39:01 +0200

    Mod: Remove old debug print.

--- standalone/main_onehot_tag.py
+++ standalone/main_onehot_tag.py
@@ -199,10 +199,6 @@ def main():
 	check_args(args)
 
 	print("Start match_onehot. (suffix: \"%s\")" % args.suffix)
-
-	print("Arguments : ")
-	print(json.dumps(args.__dict__, indent="\t"))
-
 	print(" - dataset_name: %s" % args.dataset_name)
 	print(" - start_date: %s" % start_date)
 
